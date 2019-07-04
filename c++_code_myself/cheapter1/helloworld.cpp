75e8587779dce5a1e6aba5fbb08ea3c37828bfc2
hbr <2645330483@qq.com>
2019-07-04 04:30:24 -0400

    第0节

---
+++ c++_code_myself/cheapter1/helloworld.cpp
@@ -0,0 +1,42 @@
+/*
+#include<iostream>
+
+ 
+ *std::cout类型是std::iostream 
+ std::endl 是一个控制器:如果把一个控制器写进流中,就可以控制流.
+ ::称为作用域运算符
+ 命名空间是一种把相关名称聚集在一起的技术,来自标准库的名称是在名为std的名字空间中定义的.
+
+
+
+ 
+ int main()
+{
+	std::cout << "helloworld" << std::endl;	// << 左结合
+	std::cout << "this (\") is a quote , and this (\\) is a backlash";
+	return 0;
+
+}
+*/
+//习题0-6
+/*
+#include<sstream>
+int main()
+{
+	{
+		{
+
+		std::cout << "helloworld" << std::endl;	// << 左结合
+		}
+	}
+}
+*/
+
+//习题0-7
+#include<iostream>
+
+int main()
+{
+    	std::cout << "Does this work?" << std::endl;
+	return 0;
+}

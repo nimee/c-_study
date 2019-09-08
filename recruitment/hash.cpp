7a4bd5469b501836765dc88569d99cbe36927684
nibeihou <nibeihou@gmail.com>
2019-09-08 21:46:32 +0800

    两数之和

---
+++ recruitment/hash.cpp
@@ -0,0 +1,1 @@
+//https://blog.csdn.net/duan19920101/article/details/51579136   哈希表原理
---
+++ recruitment/twoSum.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<tr1/unordered_map>
+
+#include<vector>
+using namespace std;
+using namespace std::tr1;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> record;
+        for(int i = 0 ; i < nums.size() ; i ++){
+       
+            int complement = target - nums[i];
+            if(record.find(complement) != record.end()){
+                int res[] = {i, record[complement]};
+                return vector<int>(res, res + 2);
+            }
+
+            record[nums[i]] = i;
+        }
+    }
+};
+int main()
+{
+vector<int> num2;
+
+        int j;
+
+while(cin>>j)
+{
+        num2.push_back(j);
+}
+vector<int> result;
+Solution solu;
+int tar=9;
+result=solu.twoSum(num2,tar);
+cout << result[0]<< endl;
+cout << result[1]<< endl;
+return 0;
+}
